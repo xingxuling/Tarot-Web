86042d22e9add3522690cc7160a5149756e48479
Han <1655466387@qq.com>
2024-12-05 18:24:03 +0800

    添加开源信息🍎

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,6 +3,7 @@
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-6 md:gap-16 w-full max-w-[1666px] border-t">
       <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400">
         <p>塔罗牌占卜 由 <a href="https://www.vvhan.com" target="_blank" title="韩小韩博客">韩小韩博客</a> 提供技术支持</p>
+        <p>开源地址 <a href="https://github.com/uxiaohan/Tarot-Web" target="_blank" title="Tarot-Web">Tarot-Web</a></p>
       </section>
       <section class="flex flex-col items-center gap-2">
         <div class="flex gap-6">
