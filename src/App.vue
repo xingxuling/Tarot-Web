2cd2f8501acbcc6c52a6b10cbf68a6c5b8b2ffbe
Han <1655466387@qq.com>
2024-12-05 18:20:18 +0800

    塔罗🍊

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header title="塔罗牌占卜" desc="仅供娱乐" />
+  <main><RouterView /></main>
+  <Footer />
+</template>
+<script setup lang="ts">
+import Header from '@/components/Header/Header.vue'
+import Footer from '@/components/Footer/Footer.vue'
+</script>
+
+<style scoped></style>
